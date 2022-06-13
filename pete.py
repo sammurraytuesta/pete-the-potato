d00ec9db8814050e71bbd68776d51fec3d4827bf
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2022-06-13 00:49:17 -0700

    heroku token

--- pete.py
+++ pete.py
@@ -1,8 +1,13 @@
 # bot.py
+import os
 import random
 import discord
+from dotenv import load_dotenv
 
 from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!',intents=discord.Intents.default())
 
